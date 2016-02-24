82e4a5d2541cd40867dc138c47bbe7960bf72360
a.poptsov <alexey.poptsov@kandasoft.com>
2016-02-25 01:37:14 +0300

    add test to the formatter

--- intro/build.sbt
+++ intro/build.sbt
@@ -3,4 +3,8 @@ name := "intro"
 version := "1.0"
 
 scalaVersion := "2.11.7"
-    
+
+resolvers += "Artima Maven Repository" at "http://repo.artima.com/releases"
+
+libraryDependencies += "org.scalactic" %% "scalactic" % "3.0.0-M15"
+libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.0-M15" % "test"
---
+++ intro/src/test/scala/clusterization/service/formatter/DefaultClusterisationFormatter$Test.scala
@@ -0,0 +1,16 @@
+package clusterization.service.formatter
+
+import org.scalatest._
+
+/**
+  * @author Alexey Poptsov
+  */
+class DefaultClusterisationFormatter$Test extends FlatSpec with Matchers {
+
+  "A Formatter" should "separate transaction and segment using \\t" in {
+    val formatter = DefaultClusterisationFormatter
+    val result = formatter.format("id", "name")
+    result should be("id\tname")
+  }
+
+}
